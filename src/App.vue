6521a9c8e5fee862e3a704474ae50fc5278e512c
LzhengH <864345220@qq.com>
2020-07-20 23:02:42 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <div id="nav">
+        <span class="test">ABCDE</span>
+    </div>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+  export default {}
+  // rem计算
+  function recalc () {
+    const html = document.querySelector('html')
+    let fontSize = window.innerWidth / 10
+    fontSize = fontSize > 50 ? 50 : fontSize
+    html.style.fontSize = fontSize + 'px'
+  }
+  document.addEventListener('DOMContentLoaded', recalc, false)
+  window.addEventListener('resize', recalc, false)
+</script>
+<style lang='scss' scope>
+  @import './assets/styles/global.scss';
+  .test {
+    font-size: px2rem(20);
+    color: red;
+  }
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="home">
+    <span class="icon-add"></span>
+    <span class="icon-clock"></span>
+
+  </div>
+</template>
+
+<script>
+  import Epub from 'epubjs'
+
+  global.epub = Epub
+  export default {
+    name: 'Home',
+    mounted () {
+    }
+  }
+</script>
+<style scope>
+
+</style>
